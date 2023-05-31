686a0dc25e615f7eeeaa0648460297b14822b538
Maciej Nesluchowski <mati.n200073@gmail.com>
2023-05-31 22:52:35 +0200

    functions added

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let bodyElement = document.querySelector(".js-body");
-let buttonElement = document.querySelector(".js-button");
-let aboutMeElement = document.querySelector(".js-aboutMe");
-let headerElement = document.querySelector(".js-header");
+{
+    let bodyElement = document.querySelector(".js-body");
+    let buttonElement = document.querySelector(".js-button");
+    let aboutMeElement = document.querySelector(".js-aboutMe");
+    let headerElement = document.querySelector(".js-header");
+
+    const onChangeBackgroundClick = () => {
+        bodyElement.classList.toggle("body--greyBackground");
+        aboutMeElement.classList.toggle("darkAboutMe");
+        buttonElement.classList.toggle("darkButton");
+        headerElement.classList.toggle("darkHeader");
+
+        buttonElement.innerText = bodyElement.classList.contains("body--greyBackground") ? "Białe tło" : "Szare tło"
+    }
 
 
+    buttonElement.addEventListener("click", onChangeBackgroundClick);
 
-buttonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--greyBackground");
-    aboutMeElement.classList.toggle("darkAboutMe");
-    buttonElement.classList.toggle("darkButton");
-    headerElement.classList.toggle("darkHeader");
-
-    buttonElement.innerText = bodyElement.classList.contains("body--greyBackground") ? "Białe tło" : "Szare tło"
-});
+}
